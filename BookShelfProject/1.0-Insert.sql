b3eb61d04d0dd9dbba0f68c6dba93c9154571203
G3Developers <g3devs@gmail.com>
2018-03-29 14:11:11 +0200

    4 exaple inserts (not verified)

---
+++ BookShelfProject/1.0-Insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO BOOK 
+(TITLE,AUTHOR,ISBN,READ,DESCRIPTION,COMMENTS,PRICE,RELEASE_DATE, BOOKMARK, GENRE, PUBLISHER) 
+VALUES
+('Pisma ascetyczne. T.1','Ewagriusz z Pontu','9788373541795',false,'Test Description','Test Comment',51.00,'2012-08-17',null, 'teologia', 'Wydawnictwo Benedyktynów Tyniec'),
+('Adept','Adam Przechrzta','9788379641512',true,null,null,26.76,'2016-04-13',null,'fantasy','Fabryka Słów'),
+('Demony Leningradu','Adam Przechrzta','9788375745863',true,24.21,'2011-09-16',null,'fantasy','Fabryka Słów'),
+('Szczury Wrocławia. Chaos','Robert J. Szmidt','9788363944810',true,null,null,25.06,'2015-04-22',null,'zombie','Insignis');
