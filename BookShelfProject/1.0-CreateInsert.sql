328a75c1761ee28b69a25859b1705a6723cf721b
jck9719 <gaunt1111@gmail.com>
2018-04-04 17:43:56 +0200

    create and insert merged into 1 file, added drop on first line

---
+++ BookShelfProject/1.0-CreateInsert.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS BOOK;
+
+CREATE TABLE IF NOT EXISTS BOOK(
+  ID_BOOK SERIAL PRIMARY KEY,
+  TITLE VARCHAR(64) NOT NULL,
+  AUTHOR VARCHAR(64) NOT NULL,
+  ISBN ISBN,
+  READ BOOLEAN NOT NULL,
+  DESCRIPTION TEXT,
+  COMMENTS TEXT,
+  PRICE REAL CHECK(PRICE>0),
+  RELEASE_DATE DATE,
+  BOOKMARK TEXT,
+  GENRE VARCHAR(64),
+  PUBLISHER VARCHAR(64)
+);
+
+INSERT INTO BOOK (TITLE,AUTHOR,ISBN,READ,DESCRIPTION,COMMENTS,PRICE,RELEASE_DATE, BOOKMARK, GENRE, PUBLISHER) 
+VALUES
+('Pisma ascetyczne. T.1','Ewagriusz z Pontu','9788373541795',false,'Test Description','Test Comment',51.00,'2012-08-17',null, 'teologia', 'Wydawnictwo Benedyktynów Tyniec'),
+('Adept','Adam Przechrzta','9788379641512',true,null,null,26.76,'2016-04-13',null,'fantasy','Fabryka Słów'),
+('Demony Leningradu','Adam Przechrzta','9788375745863',true,null,null,24.21,'2011-09-16',null,'fantasy','Fabryka Słów'),
+('Szczury Wrocławia. Chaos','Robert J. Szmidt','9788363944810',true,null,null,25.06,'2015-04-22',null,'zombie','Insignis');
